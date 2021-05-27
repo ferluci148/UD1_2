2b6488de9ffecbb6727f0e4b1e0af8b7b2d9d419
Fernando Lucia <fernando@fpsantacatalina.com>
2021-05-27 19:57:25 +0200

    Agregar archivos de proyecto.

---
+++ Consola/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("Hola Mundo !!!!!!!!!!!!"  );
+             
+
+
+            Console.ReadKey();
+        }
+    }
+}
